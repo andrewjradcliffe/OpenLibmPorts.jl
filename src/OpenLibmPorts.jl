6aa55a5c362b56da7b56b6a54f9b6763f1434db9
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-10-26 21:20:17 -0700

    Add interface to match that of SpecialFunctions

--- src/OpenLibmPorts.jl
+++ src/OpenLibmPorts.jl
@@ -2,6 +2,9 @@ module OpenLibmPorts
 
 # Write your package code here.
 
+include("e_lgamma_r.jl")
+include("e_lgammaf_r.jl")
+
 export lgamma
 
 lgamma(x::Number) = lgamma(float(x))
@@ -9,5 +12,20 @@ lgamma(x::Float64) = lgamma_r(x)
 lgamma(x::Float32) = lgammaf_r(x)
 lgamma(x::Float16) = Float16(lgammaf_r(Float32(x)))
 
+loggamma(x::Number) = loggamma(float(x))
+function loggamma(x::Float64)
+    u = reinterpret(UInt64, x)
+    hx = signed((u >>> 32) % UInt32)
+    ix = signed(hx & 0x7fffffff)
+    hx < 0 && (ix < 0x3b900000 || ix ≥ 0x43300000) && throw(DomainError(x, "`gamma(x)` must be non-negative"))
+    lgamma(x)
+end
+function loggamma(x::Float64)
+    hx = reinterpret(Int32, x)
+    ix = signed(hx & 0x7fffffff)
+    hx < 0 && (ix < 0x35000000 || ix ≥ 0x4b000000) && throw(DomainError(x, "`gamma(x)` must be non-negative"))
+    lgamma(x)
+end
+loggamma(x::Float16) = Float16(loggamma(Float32(x)))
 
 end
