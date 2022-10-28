c43916a6291aeb43c05c8b0bed91ad6e1bba2d5a
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-10-27 21:28:53 -0700

    Clean up package definition

--- src/OpenLibmPorts.jl
+++ src/OpenLibmPorts.jl
@@ -12,7 +12,10 @@ loggamma(x::Float16) = Float16(loggammaf_r(Float32(x)))
 
 
 # Matches behavior of SpecialFunctions' logabsgamma
-_logabsgamma(x::Float64) = lgamma_r(x)
+function _logabsgamma(x::Float64)
+    y, s = lgamma_r(x)
+    y, Int(s)
+end
 function _logabsgamma(x::Float32)
     y, s = lgammaf_r(x)
     y, Int(s)
@@ -30,23 +33,6 @@ logabsgamma(x::Base.IEEEFloat) = _logabsgamma(x)
 Equivalent functions from SpecialFunctions, defined using the above drop-in
 replacements. Why bother? Pure Julia, hence, differentiable without
 ChainRules, ChainRulesCore, etc.
-
-Why bother, you might ask? In the construction of Bayesian models,
-certain special functions may show up in the likelihood (!!!
-e.g. loggamma in negative binomial), which means that considerable
-latency can be introduced by marginally slower special functions. One
-might be inclined to be incredulous at such a statement, but consider
-that a log-likelihood computation is essentially a sum involving M
-terms, i.e. ∑ᵢ₌₁ᴹ f(xᵢ). This cost is linear in the number of terms,
-thus, we have M evals of f. If we assume the cost of summation itself
-to be negligible (it's not), then the cost of the log-likelihood is
-equal to M times the cost of f. In this light, any reduction to the
-cost of f translates directly to an equivalent reduction in the
-log-likelihood computation. As the log-likelihood is most often the
-dominant cost in a probabilistic model (which does not involve ODEs),
-reductions thereof translate directly to reductions in overall time
-required to fit the model.
-
 =#
 
 ####
