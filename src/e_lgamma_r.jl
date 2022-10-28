8befd2d182b53580e0f4f067a5ecae27d05f995d
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-10-27 18:51:19 -0700

    Consolidate revisions and remove drafts

--- src/e_lgamma_r.jl
+++ src/e_lgamma_r.jl
@@ -151,17 +151,17 @@ const w6  = -1.63092934096575273989e-03 #= 0xBF5AB89D, 0x0B9E43E4 =#
 # Matches OpenLibm behavior exactly, including return of sign
 function lgamma_r(x::Float64)
     u = reinterpret(UInt64, x)
-    hx = signed((u >>> 32) % UInt32)
-    lx = signed(u % UInt32)
+    hx = (u >>> 32) % Int32
+    lx = u % Int32
 
     #= purge off +-inf, NaN, +-0, tiny and negative arguments =#
-    signgamp = 1
+    signgamp = Int32(1)
     ix = signed(hx & 0x7fffffff)
     ix ≥ 0x7ff00000 && return x * x, signgamp
     ix | lx == 0 && return 1.0 / 0.0, signgamp
     if ix < 0x3b900000 #= |x|<2**-70, return -log(|x|) =#
         if hx < 0
-            signgamp = -1
+            signgamp = Int32(-1)
             return -log(-x), signgamp
         else
             return -log(x), signgamp
@@ -172,366 +172,7 @@ function lgamma_r(x::Float64)
         t = sinpi(x)
         t == 0.0 && return 1.0 / 0.0, signgamp #= -integer =#
         nadj = log(π / abs(t * x))
-        if t < 0; signgamp = -1; end
-        x = -x
-    end
-
-    #= purge off 1 and 2 =#
-    if ((ix - 0x3ff00000) | lx) == 0 || ((ix - 0x40000000) | lx) == 0
-        r = 0.0
-        #= for x < 2.0 =#
-    elseif ix < 0x40000000
-        if ix ≤ 0x3feccccc #= lgamma(x) = lgamma(x+1)-log(x) =#
-            r = -log(x)
-            if ix ≥ 0x3FE76944
-                y = 1.0 - x
-                i = 0
-            elseif ix ≥ 0x3FCDA661
-                y = x - (tc - 1.0)
-                i = 1
-            else
-                y = x
-                i = 2
-            end
-        else
-            r = 0.0
-            if ix ≥ 0x3FFBB4C3 #= [1.7316,2] =#
-                y = 2.0 - x
-                i = 0
-            elseif ix ≥ 0x3FF3B4C4 #= [1.23,1.73] =#
-                y = x - tc
-                i = 1
-            else
-                y = x - 1.0
-                i = 2
-            end
-        end
-        if i == 0
-            z = y*y;
-		    p1 = a0+z*(a2+z*(a4+z*(a6+z*(a8+z*a10))));
-		    p2 = z*(a1+z*(a3+z*(a5+z*(a7+z*(a9+z*a11)))));
-		    p  = y*p1+p2;
-		    r  += (p-0.5*y);
-        elseif i == 1
-            z = y*y;
-		    w = z*y;
-		    p1 = t0+w*(t3+w*(t6+w*(t9 +w*t12)));	#= parallel comp =#
-		    p2 = t1+w*(t4+w*(t7+w*(t10+w*t13)));
-		    p3 = t2+w*(t5+w*(t8+w*(t11+w*t14)));
-		    p  = z*p1-(tt-w*(p2+y*p3));
-		    r += (tf + p)
-        elseif i == 2
-            p1 = y*(u0+y*(u1+y*(u2+y*(u3+y*(u4+y*u5)))));
-		    p2 = 1.0+y*(v1+y*(v2+y*(v3+y*(v4+y*v5))));
-		    r += (-0.5*y + p1/p2);
-        end
-    elseif ix < 0x40200000                #= x < 8.0 =#
-        i = trunc(Int, x)
-        y = x - float(i)
-        p = y*(s0+y*(s1+y*(s2+y*(s3+y*(s4+y*(s5+y*s6))))));
-	    q = 1.0+y*(r1+y*(r2+y*(r3+y*(r4+y*(r5+y*r6)))));
-	    r = 0.5*y+p/q;
-	    z = 1.0;	#= lgamma(1+s) = log(s) + lgamma(s) =#
-        if i == 7
-            z *= (y + 6.0)
-            @goto case6
-        elseif i == 6
-            @label case6
-            z *= (y + 5.0)
-            @goto case5
-        elseif i == 5
-            @label case5
-            z *= (y + 4.0)
-            @goto case4
-        elseif i == 4
-            @label case4
-            z *= (y + 3.0)
-            @goto case3
-        elseif i == 3
-            @label case3
-            z *= (y + 2.0)
-        end
-        r += log(z)
-        #= 8.0 ≤ x < 2^58 =#
-    elseif ix < 0x43900000
-        t = log(x)
-        z = 1.0 / x
-        y = z * z
-        w = w0+z*(w1+y*(w2+y*(w3+y*(w4+y*(w5+y*w6)))));
-	    r = (x-0.5)*(t-1.0)+w;
-    else
-        #= 2^58 ≤ x ≤ Inf =#
-        r = x * (log(x) - 1.0)
-    end
-    if hx < 0
-        r = nadj - r
-    end
-    return r, signgamp
-end
-
-# Deviates from OpenLibm: throws instead of returning negative sign; approximately 25% faster
-# when sign is not needed in subsequent computations.
-function loggamma_r(x::Float64)
-    u = reinterpret(UInt64, x)
-    hx = signed((u >>> 32) % UInt32)
-    lx = signed(u % UInt32)
-
-    #= purge off +-inf, NaN, +-0, tiny and negative arguments =#
-    ix = signed(hx & 0x7fffffff)
-    ix ≥ 0x7ff00000 && return x * x
-    ix | lx == 0 && return 1.0 / 0.0
-    if ix < 0x3b900000 #= |x|<2**-70, return -log(|x|) =#
-        if hx < 0
-            # return -log(-x)
-            throw(DomainError(x, "`gamma(x)` must be non-negative"))
-        else
-            return -log(x)
-        end
-    end
-    if hx < 0
-        ix ≥ 0x43300000 && return 1.0 / 0.0 #= |x|>=2**52, must be -integer =#
-        t = sinpi(x)
-        t == 0.0 && return 1.0 / 0.0 #= -integer =#
-        nadj = log(π / abs(t * x))
-        t < 0 && throw(DomainError(x, "`gamma(x)` must be non-negative"))
-        x = -x
-    end
-
-    #= purge off 1 and 2 =#
-    if ((ix - 0x3ff00000) | lx) == 0 || ((ix - 0x40000000) | lx) == 0
-        r = 0.0
-        #= for x < 2.0 =#
-    elseif ix < 0x40000000
-        if ix ≤ 0x3feccccc #= lgamma(x) = lgamma(x+1)-log(x) =#
-            r = -log(x)
-            if ix ≥ 0x3FE76944
-                y = 1.0 - x
-                i = 0
-            elseif ix ≥ 0x3FCDA661
-                y = x - (tc - 1.0)
-                i = 1
-            else
-                y = x
-                i = 2
-            end
-        else
-            r = 0.0
-            if ix ≥ 0x3FFBB4C3 #= [1.7316,2] =#
-                y = 2.0 - x
-                i = 0
-            elseif ix ≥ 0x3FF3B4C4 #= [1.23,1.73] =#
-                y = x - tc
-                i = 1
-            else
-                y = x - 1.0
-                i = 2
-            end
-        end
-        if i == 0
-            z = y*y;
-		    p1 = a0+z*(a2+z*(a4+z*(a6+z*(a8+z*a10))));
-		    p2 = z*(a1+z*(a3+z*(a5+z*(a7+z*(a9+z*a11)))));
-		    p  = y*p1+p2;
-		    r  += (p-0.5*y);
-        elseif i == 1
-            z = y*y;
-		    w = z*y;
-		    p1 = t0+w*(t3+w*(t6+w*(t9 +w*t12)));	#= parallel comp =#
-		    p2 = t1+w*(t4+w*(t7+w*(t10+w*t13)));
-		    p3 = t2+w*(t5+w*(t8+w*(t11+w*t14)));
-		    p  = z*p1-(tt-w*(p2+y*p3));
-		    r += (tf + p)
-        elseif i == 2
-            p1 = y*(u0+y*(u1+y*(u2+y*(u3+y*(u4+y*u5)))));
-		    p2 = 1.0+y*(v1+y*(v2+y*(v3+y*(v4+y*v5))));
-		    r += (-0.5*y + p1/p2);
-        end
-    elseif ix < 0x40200000                #= x < 8.0 =#
-        i = trunc(Int, x)
-        y = x - float(i)
-        p = y*(s0+y*(s1+y*(s2+y*(s3+y*(s4+y*(s5+y*s6))))));
-	    q = 1.0+y*(r1+y*(r2+y*(r3+y*(r4+y*(r5+y*r6)))));
-	    r = 0.5*y+p/q;
-	    z = 1.0;	#= lgamma(1+s) = log(s) + lgamma(s) =#
-        if i == 7
-            z *= (y + 6.0)
-            @goto case6
-        elseif i == 6
-            @label case6
-            z *= (y + 5.0)
-            @goto case5
-        elseif i == 5
-            @label case5
-            z *= (y + 4.0)
-            @goto case4
-        elseif i == 4
-            @label case4
-            z *= (y + 3.0)
-            @goto case3
-        elseif i == 3
-            @label case3
-            z *= (y + 2.0)
-        end
-        r += log(z)
-        #= 8.0 ≤ x < 2^58 =#
-    elseif ix < 0x43900000
-        t = log(x)
-        z = 1.0 / x
-        y = z * z
-        w = w0+z*(w1+y*(w2+y*(w3+y*(w4+y*(w5+y*w6)))));
-	    r = (x-0.5)*(t-1.0)+w;
-    else
-        #= 2^58 ≤ x ≤ Inf =#
-        r = x * (log(x) - 1.0)
-    end
-    if hx < 0
-        r = nadj - r
-    end
-    return r
-end
-
-function loggamma_r2(x::Float64)
-    u = reinterpret(UInt64, x)
-    hx = (u >>> 32) % Int32
-    lx = u % Int32
-
-    #= purge off +-inf, NaN, +-0, tiny and negative arguments =#
-    ix = signed(hx & 0x7fffffff)
-    ix ≥ 0x7ff00000 && return x * x
-    ix | lx == 0 && return 1.0 / 0.0
-    if ix < 0x3b900000 #= |x|<2**-70, return -log(|x|) =#
-        if hx < 0
-            # return -log(-x)
-            throw(DomainError(x, "`gamma(x)` must be non-negative"))
-        else
-            return -log(x)
-        end
-    end
-    if hx < 0
-        ix ≥ 0x43300000 && return 1.0 / 0.0 #= |x|>=2**52, must be -integer =#
-        t = sinpi(x)
-        t == 0.0 && return 1.0 / 0.0 #= -integer =#
-        nadj = log(π / abs(t * x))
-        t < 0 && throw(DomainError(x, "`gamma(x)` must be non-negative"))
-        x = -x
-    end
-
-    #= purge off 1 and 2 =#
-    if ((ix - 0x3ff00000) | lx) == 0 || ((ix - 0x40000000) | lx) == 0
-        r = 0.0
-        #= for x < 2.0 =#
-    elseif ix < 0x40000000
-        if ix ≤ 0x3feccccc #= lgamma(x) = lgamma(x+1)-log(x) =#
-            r = -log(x)
-            if ix ≥ 0x3FE76944
-                y = 1.0 - x
-                i = Int32(0)
-            elseif ix ≥ 0x3FCDA661
-                y = x - (tc - 1.0)
-                i = Int32(1)
-            else
-                y = x
-                i = Int32(2)
-            end
-        else
-            r = 0.0
-            if ix ≥ 0x3FFBB4C3 #= [1.7316,2] =#
-                y = 2.0 - x
-                i = Int32(0)
-            elseif ix ≥ 0x3FF3B4C4 #= [1.23,1.73] =#
-                y = x - tc
-                i = Int32(1)
-            else
-                y = x - 1.0
-                i = Int32(2)
-            end
-        end
-        if i == Int32(0)
-            z = y*y;
-		    p1 = a0+z*(a2+z*(a4+z*(a6+z*(a8+z*a10))));
-		    p2 = z*(a1+z*(a3+z*(a5+z*(a7+z*(a9+z*a11)))));
-		    p  = y*p1+p2;
-		    r  += (p-0.5*y);
-        elseif i == Int32(1)
-            z = y*y;
-		    w = z*y;
-		    p1 = t0+w*(t3+w*(t6+w*(t9 +w*t12)));	#= parallel comp =#
-		    p2 = t1+w*(t4+w*(t7+w*(t10+w*t13)));
-		    p3 = t2+w*(t5+w*(t8+w*(t11+w*t14)));
-		    p  = z*p1-(tt-w*(p2+y*p3));
-		    r += (tf + p)
-        elseif i == Int32(2)
-            p1 = y*(u0+y*(u1+y*(u2+y*(u3+y*(u4+y*u5)))));
-		    p2 = 1.0+y*(v1+y*(v2+y*(v3+y*(v4+y*v5))));
-		    r += (-0.5*y + p1/p2);
-        end
-    elseif ix < 0x40200000                #= x < 8.0 =#
-        i = Base.unsafe_trunc(Int32, x)
-        y = x - float(i)
-        p = y*(s0+y*(s1+y*(s2+y*(s3+y*(s4+y*(s5+y*s6))))));
-	    q = 1.0+y*(r1+y*(r2+y*(r3+y*(r4+y*(r5+y*r6)))));
-	    r = 0.5*y+p/q;
-	    z = 1.0;	#= lgamma(1+s) = log(s) + lgamma(s) =#
-        if i == Int32(7)
-            z *= (y + 6.0)
-            @goto case6
-        elseif i == Int32(6)
-            @label case6
-            z *= (y + 5.0)
-            @goto case5
-        elseif i == Int32(5)
-            @label case5
-            z *= (y + 4.0)
-            @goto case4
-        elseif i == Int32(4)
-            @label case4
-            z *= (y + 3.0)
-            @goto case3
-        elseif i == Int32(3)
-            @label case3
-            z *= (y + 2.0)
-        end
-        r += log(z)
-        #= 8.0 ≤ x < 2^58 =#
-    elseif ix < 0x43900000
-        t = log(x)
-        z = 1.0 / x
-        y = z * z
-        w = w0+z*(w1+y*(w2+y*(w3+y*(w4+y*(w5+y*w6)))));
-	    r = (x-0.5)*(t-1.0)+w;
-    else
-        #= 2^58 ≤ x ≤ Inf =#
-        r = x * (log(x) - 1.0)
-    end
-    if hx < 0
-        r = nadj - r
-    end
-    return r
-end
-
-function loggamma_r3(x::Float64)
-    u = reinterpret(UInt64, x)
-    hx = (u >>> 32) % Int32
-    lx = u % Int32
-
-    #= purge off +-inf, NaN, +-0, tiny and negative arguments =#
-    ix = signed(hx & 0x7fffffff)
-    ix ≥ 0x7ff00000 && return x * x
-    ix | lx == 0 && return 1.0 / 0.0
-    if ix < 0x3b900000 #= |x|<2**-70, return -log(|x|) =#
-        if hx < 0
-            # return -log(-x)
-            throw(DomainError(x, "`gamma(x)` must be non-negative"))
-        else
-            return -log(x)
-        end
-    end
-    if hx < 0
-        ix ≥ 0x43300000 && return 1.0 / 0.0 #= |x|>=2**52, must be -integer =#
-        t = sinpi(x)
-        t == 0.0 && return 1.0 / 0.0 #= -integer =#
-        nadj = log(π / abs(t * x))
-        t < 0 && throw(DomainError(x, "`gamma(x)` must be non-negative"))
+        if t < 0.0; signgamp = Int32(-1); end
         x = -x
     end
 
@@ -587,9 +228,10 @@ function loggamma_r3(x::Float64)
     elseif ix < 0x40200000                #= x < 8.0 =#
         i = Base.unsafe_trunc(Int8, x)
         y = x - float(i)
-        p = y*(s0+y*(s1+y*(s2+y*(s3+y*(s4+y*(s5+y*s6))))));
-	    q = 1.0+y*(r1+y*(r2+y*(r3+y*(r4+y*(r5+y*r6)))));
-	    r = 0.5*y+p/q;
+        # If performed here, performance is 2x worse; hence, move it below.
+        # p = y*(s0+y*(s1+y*(s2+y*(s3+y*(s4+y*(s5+y*s6))))));
+	    # q = 1.0+y*(r1+y*(r2+y*(r3+y*(r4+y*(r5+y*r6)))));
+	    # r = 0.5*y+p/q;
 	    z = 1.0;	#= lgamma(1+s) = log(s) + lgamma(s) =#
         if i == Int8(7)
             z *= (y + 6.0)
@@ -610,7 +252,10 @@ function loggamma_r3(x::Float64)
             @label case3
             z *= (y + 2.0)
         end
-        r += log(z)
+        # r += log(z)
+        p = y*(s0+y*(s1+y*(s2+y*(s3+y*(s4+y*(s5+y*s6))))));
+        q = 1.0+y*(r1+y*(r2+y*(r3+y*(r4+y*(r5+y*r6)))));
+        r = log(z) + 0.5*y+p/q;
         #= 8.0 ≤ x < 2^58 =#
     elseif ix < 0x43900000
         t = log(x)
@@ -625,10 +270,12 @@ function loggamma_r3(x::Float64)
     if hx < 0
         r = nadj - r
     end
-    return r
+    return r, signgamp
 end
 
-function loggamma_r4(x::Float64)
+# Deviates from OpenLibm: throws instead of returning negative sign; approximately 25% faster
+# when sign is not needed in subsequent computations.
+function loggamma_r(x::Float64)
     u = reinterpret(UInt64, x)
     hx = (u >>> 32) % Int32
     lx = u % Int32
@@ -650,7 +297,7 @@ function loggamma_r4(x::Float64)
         t = sinpi(x)
         t == 0.0 && return 1.0 / 0.0 #= -integer =#
         nadj = log(π / abs(t * x))
-        t < 0 && throw(DomainError(x, "`gamma(x)` must be non-negative"))
+        t < 0.0 && throw(DomainError(x, "`gamma(x)` must be non-negative"))
         x = -x
     end
 
@@ -663,38 +310,34 @@ function loggamma_r4(x::Float64)
             r = -log(x)
             if ix ≥ 0x3FE76944
                 y = 1.0 - x
-                @goto _case0
+                i = Int8(0)
             elseif ix ≥ 0x3FCDA661
                 y = x - (tc - 1.0)
-                @goto _case1
+                i = Int8(1)
             else
                 y = x
-                @goto _case2
+                i = Int8(2)
             end
         else
             r = 0.0
             if ix ≥ 0x3FFBB4C3 #= [1.7316,2] =#
                 y = 2.0 - x
-                @goto _case0
+                i = Int8(0)
             elseif ix ≥ 0x3FF3B4C4 #= [1.23,1.73] =#
                 y = x - tc
-                @goto _case1
+                i = Int8(1)
             else
                 y = x - 1.0
-                @goto _case2
+                i = Int8(2)
             end
         end
-        begin
-            @label _case0
+        if i == Int8(0)
             z = y*y;
 		    p1 = a0+z*(a2+z*(a4+z*(a6+z*(a8+z*a10))));
 		    p2 = z*(a1+z*(a3+z*(a5+z*(a7+z*(a9+z*a11)))));
 		    p  = y*p1+p2;
 		    r  += (p-0.5*y);
-            @goto _endcases
-        end
-        begin
-            @label _case1
+        elseif i == Int8(1)
             z = y*y;
 		    w = z*y;
 		    p1 = t0+w*(t3+w*(t6+w*(t9 +w*t12)));	#= parallel comp =#
@@ -702,148 +345,15 @@ function loggamma_r4(x::Float64)
 		    p3 = t2+w*(t5+w*(t8+w*(t11+w*t14)));
 		    p  = z*p1-(tt-w*(p2+y*p3));
 		    r += (tf + p)
-            @goto _endcases
-        end
-        begin
-            @label _case2
+        elseif i == Int8(2)
             p1 = y*(u0+y*(u1+y*(u2+y*(u3+y*(u4+y*u5)))));
 		    p2 = 1.0+y*(v1+y*(v2+y*(v3+y*(v4+y*v5))));
 		    r += (-0.5*y + p1/p2);
-            @goto _endcases
         end
-        @label _endcases
     elseif ix < 0x40200000                #= x < 8.0 =#
         i = Base.unsafe_trunc(Int8, x)
         y = x - float(i)
-        p = y*(s0+y*(s1+y*(s2+y*(s3+y*(s4+y*(s5+y*s6))))));
-	    q = 1.0+y*(r1+y*(r2+y*(r3+y*(r4+y*(r5+y*r6)))));
-	    r = 0.5*y+p/q;
-	    z = 1.0;	#= lgamma(1+s) = log(s) + lgamma(s) =#
-        if i == Int8(7)
-            z *= (y + 6.0)
-            @goto case6
-        elseif i == Int8(6)
-            @label case6
-            z *= (y + 5.0)
-            @goto case5
-        elseif i == Int8(5)
-            @label case5
-            z *= (y + 4.0)
-            @goto case4
-        elseif i == Int8(4)
-            @label case4
-            z *= (y + 3.0)
-            @goto case3
-        elseif i == Int8(3)
-            @label case3
-            z *= (y + 2.0)
-        end
-        r += log(z)
-        #= 8.0 ≤ x < 2^58 =#
-    elseif ix < 0x43900000
-        t = log(x)
-        z = 1.0 / x
-        y = z * z
-        w = w0+z*(w1+y*(w2+y*(w3+y*(w4+y*(w5+y*w6)))));
-	    r = (x-0.5)*(t-1.0)+w;
-    else
-        #= 2^58 ≤ x ≤ Inf =#
-        r = x * (log(x) - 1.0)
-    end
-    if hx < 0
-        r = nadj - r
-    end
-    return r
-end
-
-
-function loggamma_r5(x::Float64)
-    u = reinterpret(UInt64, x)
-    hx = (u >>> 32) % Int32
-    lx = u % Int32
-
-    #= purge off +-inf, NaN, +-0, tiny and negative arguments =#
-    ix = signed(hx & 0x7fffffff)
-    ix ≥ 0x7ff00000 && return x * x
-    ix | lx == 0 && return 1.0 / 0.0
-    if ix < 0x3b900000 #= |x|<2**-70, return -log(|x|) =#
-        if hx < 0
-            # return -log(-x)
-            throw(DomainError(x, "`gamma(x)` must be non-negative"))
-        else
-            return -log(x)
-        end
-    end
-    if hx < 0
-        ix ≥ 0x43300000 && return 1.0 / 0.0 #= |x|>=2**52, must be -integer =#
-        t = sinpi(x)
-        t == 0.0 && return 1.0 / 0.0 #= -integer =#
-        nadj = log(π / abs(t * x))
-        t < 0 && throw(DomainError(x, "`gamma(x)` must be non-negative"))
-        x = -x
-    end
-
-    #= purge off 1 and 2 =#
-    if ((ix - 0x3ff00000) | lx) == 0 || ((ix - 0x40000000) | lx) == 0
-        r = 0.0
-        #= for x < 2.0 =#
-    elseif ix < 0x40000000
-        if ix ≤ 0x3feccccc #= lgamma(x) = lgamma(x+1)-log(x) =#
-            r = -log(x)
-            if ix ≥ 0x3FE76944
-                y = 1.0 - x
-                @goto _case0
-            elseif ix ≥ 0x3FCDA661
-                y = x - (tc - 1.0)
-                @goto _case1
-            else
-                y = x
-                @goto _case2
-            end
-        else
-            r = 0.0
-            if ix ≥ 0x3FFBB4C3 #= [1.7316,2] =#
-                y = 2.0 - x
-                @goto _case0
-            elseif ix ≥ 0x3FF3B4C4 #= [1.23,1.73] =#
-                y = x - tc
-                @goto _case1
-            else
-                y = x - 1.0
-                @goto _case2
-            end
-        end
-        begin
-            @label _case0
-            z = y*y;
-		    p1 = a0+z*(a2+z*(a4+z*(a6+z*(a8+z*a10))));
-		    p2 = z*(a1+z*(a3+z*(a5+z*(a7+z*(a9+z*a11)))));
-		    p  = y*p1+p2;
-		    r  += (p-0.5*y);
-            @goto _endcases
-        end
-        begin
-            @label _case1
-            z = y*y;
-		    w = z*y;
-		    p1 = t0+w*(t3+w*(t6+w*(t9 +w*t12)));	#= parallel comp =#
-		    p2 = t1+w*(t4+w*(t7+w*(t10+w*t13)));
-		    p3 = t2+w*(t5+w*(t8+w*(t11+w*t14)));
-		    p  = z*p1-(tt-w*(p2+y*p3));
-		    r += (tf + p)
-            @goto _endcases
-        end
-        begin
-            @label _case2
-            p1 = y*(u0+y*(u1+y*(u2+y*(u3+y*(u4+y*u5)))));
-		    p2 = 1.0+y*(v1+y*(v2+y*(v3+y*(v4+y*v5))));
-		    r += (-0.5*y + p1/p2);
-            @goto _endcases
-        end
-        @label _endcases
-    elseif ix < 0x40200000                #= x < 8.0 =#
-        i = Base.unsafe_trunc(Int8, x)
-        y = x - float(i)
+        # If performed here, performance is 2x worse; hence, move it below.
         # p = y*(s0+y*(s1+y*(s2+y*(s3+y*(s4+y*(s5+y*s6))))));
 	    # q = 1.0+y*(r1+y*(r2+y*(r3+y*(r4+y*(r5+y*r6)))));
 	    # r = 0.5*y+p/q;
