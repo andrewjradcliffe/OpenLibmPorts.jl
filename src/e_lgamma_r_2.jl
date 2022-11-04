7dedfb326cc013e4b9838a1a0d39e3e9de0c3f71
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-11-04 07:29:39 -0700

    Revised version of `Float32`

--- src/e_lgamma_r_2.jl
+++ src/e_lgamma_r_2.jl
@@ -21,29 +21,28 @@ function _loggamma3(x::Float64)
     #= purge off +-inf, NaN, +-0, tiny and negative arguments =#
     signgamp = Int32(1)
     ix = hx & 0x7fffffff
-    ix ≥ 0x7ff00000 && return typemax(x), signgamp  # isinf(x)
-    ix | lx == 0x00000000 && return typemax(x), signgamp # iszero(x)
+    ix ≥ 0x7ff00000 && return Inf, signgamp
+    ix | lx == 0x00000000 && return Inf, signgamp
     if ix < 0x3b900000 #= |x|<2**-70, return -log(|x|) =#
-        if hx < 0 # x < 0
+        if hx < Int32(0) # x < 0
             signgamp = Int32(-1)
             return -log(-x), signgamp
         else
             return -log(x), signgamp
         end
     end
-    if hx < 0
-        # ix ≥ 0x43300000 && return typemax(x), signgamp #= |x|>=2**52, must be -integer =#
+    if hx < Int32(0)
+        # ix ≥ 0x43300000 && return Inf, signgamp #= |x|>=2**52, must be -integer =#
         t = sinpi(x)
-        iszero(t) && return typemax(x), signgamp #= -integer =#
+        iszero(t) && return Inf, signgamp #= -integer =#
         nadj = log(π / abs(t * x))
         if t < 0.0; signgamp = Int32(-1); end
         x = -x
     end
-    if ix ≤ 0x40000000     #= for x < 2.0 =#
-        # fpart, ipart = modf(x)
+    if ix ≤ 0x40000000     #= for 1.0 ≤ x ≤ 2.0 =#
         ipart = round(x, RoundToZero)
         fpart = x - ipart
-        if iszero(fpart)
+        if iszero(fpart) #= purge off 1 and 2 =#
             return 0.0, signgamp
         elseif isone(ipart)
             r = 0.0
@@ -52,22 +51,20 @@ function _loggamma3(x::Float64)
             r = -log(x)
             c = 0.0
         end
-        if fpart ≥ 0.7316
-            y = 1.0 + c - x#(1.0 + _y) - x
+        if fpart ≥ 0.7315998077392578
+            y = 1.0 + c - x
             z = y * y
             p1 = evalpoly(z, (7.72156649015328655494e-02, 6.73523010531292681824e-02, 7.38555086081402883957e-03, 1.19270763183362067845e-03, 2.20862790713908385557e-04, 2.52144565451257326939e-05))
             p2 = z * evalpoly(z, (3.22467033424113591611e-01, 2.05808084325167332806e-02, 2.89051383673415629091e-03, 5.10069792153511336608e-04, 1.08011567247583939954e-04, 4.48640949618915160150e-05))
             p = muladd(p1, y, p2)
             r += muladd(y, -0.5, p)
-        elseif fpart ≥ 0.2316399812698364 #0.2316322326660156 # 0.23163999999
-            y = x - 0.46163214496836225 - c #x - (tc - (1.0 - _y))
+        elseif fpart ≥ 0.2316399812698364 # or, the lb? 0.2316322326660156
+            y = x - 0.46163214496836225 - c
             z = y * y
             w = z * y
             p1 = evalpoly(w, (4.83836122723810047042e-01, -3.27885410759859649565e-02, 6.10053870246291332635e-03, -1.40346469989232843813e-03, 3.15632070903625950361e-04))
             p2 = evalpoly(w, (-1.47587722994593911752e-01, 1.79706750811820387126e-02, -3.68452016781138256760e-03, 8.81081882437654011382e-04, -3.12754168375120860518e-04))
             p3 = evalpoly(w, (6.46249402391333854778e-02, -1.03142241298341437450e-02, 2.25964780900612472250e-03, -5.38595305356740546715e-04, 3.35529192635519073543e-04))
-            # p = muladd(w, -muladd(p3, y, p2), -3.63867699703950536541e-18)
-            # p = muladd(z, p1, -p)
             p = muladd(z, p1, -muladd(w, -muladd(p3, y, p2), -3.63867699703950536541e-18))
             r += p - 1.21486290535849611461e-1
         else
@@ -77,7 +74,7 @@ function _loggamma3(x::Float64)
 		    r += muladd(y, -0.5, p1 / p2)
         end
     elseif ix < 0x40200000              #= x < 8.0 =#
-        i = round(x, RoundToZero) #Base.unsafe_trunc(Int8, x)
+        i = round(x, RoundToZero)
         y = x - i
 	    z = 1.0
         p = 0.0
@@ -89,7 +86,7 @@ function _loggamma3(x::Float64)
         end
         p = y * evalpoly(y, (-7.72156649015328655494e-2, 2.14982415960608852501e-1, 3.25778796408930981787e-1, 1.46350472652464452805e-1, 2.66422703033638609560e-2, 1.84028451407337715652e-3, 3.19475326584100867617e-5))
         q = evalpoly(y, (1.0, 1.39200533467621045958, 7.21935547567138069525e-1, 1.71933865632803078993e-1, 1.86459191715652901344e-2, 7.77942496381893596434e-4, 7.32668430744625636189e-6))
-        r = log(z) + muladd(0.5, y, p / q) #r = log(z) + 0.5*y + p / q
+        r = log(z) + muladd(0.5, y, p / q)
     elseif ix < 0x43900000              #= 8.0 ≤ x < 2^58 =#
         z = 1.0 / x
         y = z * z
@@ -98,7 +95,7 @@ function _loggamma3(x::Float64)
     else #= 2^58 ≤ x ≤ Inf =#
         r = muladd(x, log(x), -x)
     end
-    if hx < 0
+    if hx < Int32(0)
         r = nadj - r
     end
     return r, signgamp
